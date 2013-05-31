682a4da2024b892e5d513d617145c150387e48fb
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2013-05-30 22:03:41 -0700

    Adds modal window for creating a new thread
    * Uses Ace editor for inline editing & Marked for previewing.

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -1,3 +1,21 @@
-define(function (require) {
+define(function(require) {
+	require("bootstrap/modal");
+	require("bootstrap/tab");
+	var marked = require('marked');
+	var ace = require("ace/ace");
 
+	var editor = ace.edit('create-thread-editor');
+	editor.getSession().setMode("ace/mode/markdown");
+	editor.renderer.setShowGutter(false);
+
+
+	editor.getSession().on('change', function(e) {
+		$('#create-thread-preview').html(marked(editor.getValue()));
+	});
+
+	$('a[data-toggle="tab"]').on('shown', function(e) {
+		if( $(e.target).attr("href") == '#create-thread-editor' ) {
+			editor.focus();
+		}
+	});
 });
