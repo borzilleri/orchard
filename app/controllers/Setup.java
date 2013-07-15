dd6c8b6bf8bd098ffec6e4b0fd52469ac288e9be
Jonathan Borzilleri <jborzilleri@intrepidls.com>
2013-07-15 15:25:42 -0700

    Create Setup controller, fixes Pegdown Class file names

---
+++ app/controllers/Setup.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Setup extends Controller {
+
+	public Result index() {
+		/**
+		 * TODO: Do not run if setup already completed.
+		 * STEPS:
+		 * Create admin account.
+		 *
+		 */
+
+		return ok();
+	}
+}
